1845ba950630885f098fd79eed1cee35939638e2
Márcio Moraes <marciodaniel855555@gmail.com>
2025-08-15 15:08:33 -0300

    Update auth_controller.py

--- src/app/auth_controller.py
+++ src/app/auth_controller.py
@@ -12,23 +12,22 @@ def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        remember = request.form.get('remember')  # Checkbox "manter conectado"
+        remember = request.form.get('remember')  
 
-        # Verifica se o arquivo existe
         if not os.path.exists(USERS_FILE):
             flash('Banco de usuários não encontrado.')
             return render_template('login.html')
 
-        # Carrega os usuários
+
         with open(USERS_FILE, 'r') as f:
             users = json.load(f)
 
-        # Verifica as credenciais
+
         for user in users:
-            if user.get("username") == username and user.get("password") == password: # Verifica as credenciais
-                session['username'] = username # Salva o nome do usuário na sessão
-                session.permanent = bool(remember)  # Mantém a sessão por mais tempo
-                return redirect(url_for('routes.dashboard'))  # sucesso
+            if user.get("username") == username and user.get("password") == password: 
+                session['username'] = username 
+                session.permanent = bool(remember) 
+                return redirect(url_for('routes.dashboard')) 
 
         flash('Usuário ou senha inválidos.')
 
@@ -38,12 +37,11 @@ def login():
 def logout():
     from bot_controller import stop_bot
     session.clear()
-    # Para o bot de verdade
+
     try:
         stop_bot()
     except Exception as e:
-        pass  # Opcional: logar erro
-    # Seta running como false ao sair
+        pass  
     if os.path.exists(CONFIG_FILE):
         try:
             with open(CONFIG_FILE, 'r') as f:
@@ -52,25 +50,25 @@ def logout():
             with open(CONFIG_FILE, 'w') as f:
                 json.dump(config, f, indent=4)
         except Exception as e:
-            pass  # Opcional: logar erro
+            pass  
     return redirect(url_for('auth.login'))
 
-    # Verifica se o arquivo existe
+
     if not os.path.exists(USERS_FILE):
         flash('Banco de usuários não encontrado.')
         return render_template('login.html')
 
-    # Carrega os usuários
+
     with open(USERS_FILE, 'r') as f:
         users = json.load(f)
 
-    # Verifica as credenciais
-    for user in users:
-        if user.get("username") == username and user.get("password") == password: # Verifica as credenciais
-            session['username'] = username # Salva o nome do usuário na sessão
-            session.permanent = bool(remember)  # Mantém a sessão por mais tempo
-            return redirect(url_for('routes.dashboard'))  # sucesso
 
+    for user in users:
+        if user.get("username") == username and user.get("password") == password:
+            session['username'] = username 
+            session.permanent = bool(remember) 
+            return redirect(url_for('routes.dashboard')) 
         flash('Usuário ou senha inválidos.')
 
     return render_template('login.html')
+
